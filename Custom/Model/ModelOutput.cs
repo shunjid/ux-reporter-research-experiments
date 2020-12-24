6df674dfc55b60017627e3611afcfb00b0d4edb4
shunjid <shunjid.rahman@gmail.com>
2020-12-24 14:09:12 +0600

    Tested in seperate console application

---
+++ Custom/Model/ModelOutput.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.ML.Data;
+
+namespace Custom.Model
+{
+    public class ModelOutput
+    {
+        public float Score { get; set; }
+    }
+}
---
+++ Custom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Custom.Model;
+
+namespace Custom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModelInput sampleData = new ModelInput()
+            {
+                FirstContentfulPaint = 810F,
+                TimeToInteract = 1623.0004F,
+                ServerResponseTime = 148.574F,
+                BootUpTime = 95.844F,
+                FirstMeaningfulPaint = 810F,
+                TotalBlockingTime = 1F,
+                TotalByteWeight = 964194F,
+                FirstCpuIdle = 1582F,
+                LargestContentfulPaint = 890F,
+            };
+
+            var predictionResult = ConsumeModel.Predict(sampleData);
+
+            Console.WriteLine("Using model to make single prediction -- Comparing actual UserExperience with predicted UserExperience from sample data...\n\n");
+            Console.WriteLine($"FirstContentfulPaint: {sampleData.FirstContentfulPaint}");
+            Console.WriteLine($"TimeToInteract: {sampleData.TimeToInteract}");
+            Console.WriteLine($"ServerResponseTime: {sampleData.ServerResponseTime}");
+            Console.WriteLine($"BootUpTime: {sampleData.BootUpTime}");
+            Console.WriteLine($"FirstMeaningfulPaint: {sampleData.FirstMeaningfulPaint}");
+            Console.WriteLine($"TotalBlockingTime: {sampleData.TotalBlockingTime}");
+            Console.WriteLine($"TotalByteWeight: {sampleData.TotalByteWeight}");
+            Console.WriteLine($"FirstCpuIdle: {sampleData.FirstCpuIdle}");
+            Console.WriteLine($"LargestContentfulPaint: {sampleData.LargestContentfulPaint}");
+            Console.WriteLine($"\n\nPredicted UserExperience: {predictionResult.Score}\n\n");
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
+        }
+    }
+}
