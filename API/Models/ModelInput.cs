d92fb33ca41d04639ba3ed625d8dbdd20a77422f
shunjid <shunjid.rahman@gmail.com>
2020-12-24 14:22:01 +0600

    initiate lib for ml in api

---
+++ API/Models/ModelInput.cs
@@ -0,0 +1,46 @@
+using Microsoft.ML.Data;
+
+namespace API.Models
+{
+    public class ModelInput
+    {
+        [ColumnName("FirstContentfulPaint"), LoadColumn(0)]
+        public float FirstContentfulPaint { get; set; }
+
+
+        [ColumnName("TimeToInteract"), LoadColumn(1)]
+        public float TimeToInteract { get; set; }
+
+
+        [ColumnName("ServerResponseTime"), LoadColumn(2)]
+        public float ServerResponseTime { get; set; }
+
+
+        [ColumnName("BootUpTime"), LoadColumn(3)]
+        public float BootUpTime { get; set; }
+
+
+        [ColumnName("FirstMeaningfulPaint"), LoadColumn(4)]
+        public float FirstMeaningfulPaint { get; set; }
+
+
+        [ColumnName("TotalBlockingTime"), LoadColumn(5)]
+        public float TotalBlockingTime { get; set; }
+
+
+        [ColumnName("TotalByteWeight"), LoadColumn(6)]
+        public float TotalByteWeight { get; set; }
+
+
+        [ColumnName("FirstCpuIdle"), LoadColumn(7)]
+        public float FirstCpuIdle { get; set; }
+
+
+        [ColumnName("LargestContentfulPaint"), LoadColumn(8)]
+        public float LargestContentfulPaint { get; set; }
+
+
+        [ColumnName("UserExperience"), LoadColumn(9)]
+        public float UserExperience { get; set; }
+    }
+}
---
+++ API/Models/ModelOutput.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.ML.Data;
+
+namespace API.Models
+{
+    public class ModelOutput
+    {
+        public float Score { get; set; }
+    }
+}
